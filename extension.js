5dff477865ba9889d1b83d9d4bff498226c73c0b
Josh Piasecki <138541977+FloppyDisco@users.noreply.github.com>
2024-11-18 21:47:15 +0100

    treeview is pretty much done, or whatever

--- extension.js
+++ extension.js
@@ -1,36 +1,149 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
+const package = require("./package.json");
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const viewContainerId = package.contributes.viewsContainers.activitybar[0].id;
+const views = {
+  _activeView: "javascript",
+  get activeView(){return this._activeView},
+  set activeView(view){this._activeView = view},
+  ids: package.contributes.views[viewContainerId].map((view) => view.id),
+};
+
+const languages = views.ids.reduce((languages, id)=> ({
+  ...languages,
+  [id]: require(`./languages/${id}`)
+}),{})
+
+
+//   Toggling Groups
+// -------------------
+
+let isFlatTree;
+function setIsFlatTree(bool){
+  isFlatTree = bool;
+  vscode.commands.executeCommand("setContext", package.settings.when.isFlatTree, bool)
+  views[views.activeView].provider.refresh()
+}
+
+//   Tree
+// --------
+
+class RegexTreeProvider {
+  constructor(tokenGroups) {
+    this.tokenGroups = tokenGroups;
+    this._onDidChangeTreeData = new vscode.EventEmitter();
+    this.onDidChangeTreeData = this._onDidChangeTreeData.event;
+  }
+
+  getChildren(element) {
+    return element
+      ? element.children
+      : isFlatTree
+        ? this.tokenGroups.reduce((tokens, group) => [...tokens, ...this.getChildren(group)],[])
+        : this.tokenGroups.map(group => ({...group, state: vscode.TreeItemCollapsibleState.Collapsed}))
+  }
+
+  getTreeItem(element) {
+    return new RegexTreeItem(element)
+  }
+
+  refresh(){
+    this._onDidChangeTreeData.fire()
+  }
+}
+
+const copyToken = package.settings.commands.copyToken
+class RegexTreeItem extends vscode.TreeItem {
+  constructor({group, token, copyValue, description, state}) {
+    super(group || token, state);
+    this.token = copyValue || token
+    this.tooltip = description;
+    this.description = description;
+
+    this.command = {
+      command: copyToken,
+      arguments: [this]
+    }
+  }
+}
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  context.subscriptions.push(
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "regex-tokens" is now active!');
+    vscode.commands.registerCommand(
+      package.settings.commands.setFlatTree, () => {
+        setIsFlatTree(true);
+    }),
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('regex-tokens.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+    vscode.commands.registerCommand(
+      package.settings.commands.setNestedTree, () => {
+        setIsFlatTree(false);
+    }),
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from RegexTokens!');
-	});
+    vscode.commands.registerCommand(
+      package.settings.commands.showTokens, () => {
+        vscode.commands.executeCommand("workbench.view.extension.regexTokens")
+        vscode.commands.executeCommand("workbench.action.findInFiles")
+    }),
 
-	context.subscriptions.push(disposable);
+    vscode.commands.registerCommand(
+      package.settings.commands.copyToken, ({ token, collapsibleState }={}) => {
+        if (collapsibleState){
+          vscode.commands.executeCommand("list.expand")
+        } else {
+          if(token){
+            vscode.env.clipboard.writeText(token);
+          }
+        }
+      }
+    )
+
+  ); // subscriptions
+
+  // register all the contributed treeviews
+  for (id of views.ids) {
+    const provider = new RegexTreeProvider(languages[id]);
+    const tree = vscode.window.createTreeView(id, {
+      treeDataProvider: provider,
+    })
+
+    // for some reason the func MUST be created and closed at registration
+    // it does not work if it is generated inside onDidChangeVisibility
+    const hideOtherViews = hideViewsOtherThanId(id)
+    tree.onDidChangeVisibility((e) => hideOtherViews(e))
+
+    // store the reference to the treeviews
+    views[id] = {
+      tree,
+      provider,
+    }
+  }
+
+  function hideViewsOtherThanId(id){
+    return function ({visible}){
+      if (visible){
+        vscode.commands.executeCommand('setContext', package.settings.when.tokensVisible, visible)
+        views.activeView = id
+        views.ids.forEach(view => {
+          if (view !== views.activeView){
+            vscode.commands.executeCommand(`${view}.removeView`)
+          }
+        })
+      } else {
+        if (id === views.activeView){
+          vscode.commands.executeCommand('setContext', package.settings.when.tokensVisible, visible)
+        }
+      }
+    }
+  }
 }
 
-// This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
